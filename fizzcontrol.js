cb8ee4e8ec8a5d1ea8ca1f144942db39651cdd02
Autumn Apsey <autumnapsey@gmail.com>
2016-05-13 18:08:21 -0400

    got fizzbuzz service to print to console.

--- fizzcontrol.js
+++ fizzcontrol.js
@@ -1,34 +1,10 @@
 var app = angular.module('app');
 
-app.controller('fizzcontrol', function($scope){
-	function isDivisibleby(i,y){
-	  return i % y === 0;
-	}
-	function Search(i,y){
-	  return i.toString().indexOf(y) !== -1;
-	}
-
-	function find(i,y){
-	  return (isDivisibleby(i,y) || Search(i,y));
-	}
-
+app.controller('fizzcontrol', function($scope, fizzService){
 	$scope.fizzBuzz = function(number){
+		console.log('function is firing.');
 
-		// var num = parseFloat(number, 10);
-		// if(find(num, 3) && find(num, 5)){
-		// 	console.log("FizzBuzz");
-		// 	return "FizzBuzz";
-		
-		// }
-		// if(find(num, 3)){
-		// 	console.log("Fizz");
-		// 	return "Fizz";
-		
-		// }
-		// if(find(num, 5)){
-		// 	console.log("Buzz");
-		// 	return "Buzz";
-		
-		// }
-	}
+		fizzService.evaluateNumber(number);
+
+	};
 });
--- service.js
+++ service.js
@@ -2,6 +2,18 @@ var app = angular.module('app');
 
 app.factory('fizzService', function(){
 	var isItFizzBuzz;
+	
+	var isDivisibleby = function(i,y){
+	  return i % y === 0;
+	}
+
+	var Search = function(i,y){
+	  return i.toString().indexOf(y) !== -1;
+	}
+
+	var find = function(i,y){
+	  return (isDivisibleby(i,y) || Search(i,y));
+	}
 
 	return {
 		evaluateNumber: function(number){
@@ -9,18 +21,26 @@ app.factory('fizzService', function(){
 			if(find(num, 3) && find(num, 5)){
 				console.log("FizzBuzz");
 				isItFizzBuzz = "FizzBuzz";
+				return isItFizzBuzz;
 			
 			}
 			if(find(num, 3)){
 				console.log("Fizz");
 				isItFizzBuzz = "Fizz";
-			
+				return isItFizzBuzz;
+
 			}
 			if(find(num, 5)){
 				console.log("Buzz");
 				isItFizzBuzz = "Buzz";
-			
+				return isItFizzBuzz;
+
 			}
+			else{
+				console.log(num);
+				return num;
+			}
+
 		},
 	};
-})
+});
